78335d6c586e8d966b8f023f08afe11a91aa520b
Felipe França <contatofelipefranca12@gmail.com>
2020-09-17 21:46:11 -0300

    Adiciona nome usuário

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+            Console.Write("Digite seu sobrenome: ");
+            string sobrenome = Console.ReadLine();
+
+            string nomeCompleto = ($"{nome}{sobrenome}");
+            Console.WriteLine($"Nome completo: {nomeCompleto} ");
+            Console.WriteLine($"Nome catálogo: {sobrenome.ToUpper()}, {nome}");
+
+        }
+    }
+}
